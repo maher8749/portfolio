c65cef73e18a44135531bf4c2f79bc549b61dd2f
maher8749 <maher874969@gmail.com>
2022-11-18 14:37:24 +0545

    adding about, portfolio, blog

--- app.js
+++ app.js
@@ -4,42 +4,45 @@ const sectionBtn = document.querySelectorAll(".controls");
 const sectionBtns = document.querySelectorAll(".control");
 
 
-// button click active class
+
 function pageTransition(){
     for(let i = 0; i <= sectionBtns.length; i++){
-        sectionBtns[i].addEventListener('click', function(){
-            let currentBtn = document.querySelectorAll(".active-btn");
-            currentBtn[0].className = currentBtn[0].className.replace(" active-btn", '');
-            this.className += ' active-btn';
+        sectionBtns[i].addEventListener("click", () => {
+            document.querySelector(".active").classList.remove("active")
+            sections[i].classList.add("active")  
+            document.querySelector(".active-btn").classList.remove("active-btn")
+            sectionBtns[i].classList.add("active-btn")
         })
-
     }
-    
-    // section active 
-    allSection.addEventListener("click", (e) => {
-        const id = e.target.dataset.id;
-
-        if(id){
-            // remove selected from other btns
-            sectionBtn.forEach((btn) => {
-                btn.classList.remove('active')
-            })
-            e.target.classList.add("active")
-             
-            // hide other section 
-            sections.forEach((section) => {
-                section.classList.remove("active")
-            })
-
-            const element = document.getElementById(id)
-            element.classList.add("active")
-        }
-    })
-    
-        
 }
 
-pageTransition();
+pageTransition()
+
+
+
+
+
+
+
+
+
+// (function () {
+//     [...document.querySelectorAll(".control")].forEach(button => {
+//         button.addEventListener("click", function() {
+//             document.querySelector(".active-btn").classList.remove("active-btn");
+//             this.classList.add("active-btn");
+//             document.querySelector(".active").classList.remove("active");
+//             document.getElementById(button.dataset.id).classList.add("active");
+//         })
+//     });
+//     document.querySelector(".theme-btn").addEventListener("click", () => {
+//         document.body.classList.toggle("light-mode");
+//     })
+// })();
+
+
+
+
 
 
 
